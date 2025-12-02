4971b15b90c4995e1fa4056fc802e797d5efceff
Reet Saluja <reet.saluja@accenture.com>
2025-12-02 12:43:28 +0530

    bug-fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware   # <-- ADD THIS LINE
 from database import Base, engine
 from routers.candidate_router import router as candidate_router
 
@@ -10,5 +11,18 @@ app = FastAPI(
     version="1.0.0",
 )
 
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:3000",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,
+    allow_methods=["*"],             
+    allow_headers=["*"],
+)
 # Register router
 app.include_router(candidate_router)
